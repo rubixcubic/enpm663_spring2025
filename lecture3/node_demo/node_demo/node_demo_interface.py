e7f3c3a218231f8d5ffebb165013013184a487f5
Adon <adon.sf4@gmail.com>
2025-02-12 17:08:17 -0500

    removed previous package node_demo

--- lecture3/node_demo/node_demo/node_demo_interface.py
+++
@@ -1,7 +0,0 @@
-from rclpy.node import Node
-
-
-class NodeDemoInterface(Node):
-    def __init__(self, node_name):
-        super().__init__(node_name)
-        self.get_logger().info(f"Hello from {self.get_name()}")
