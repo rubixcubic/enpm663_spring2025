2448163e5ba03c0defc110eb7ea6494199f54ed0
Adon <adon.sf4@gmail.com>
2025-02-26 01:23:00 -0500

    added package for executors

---
+++ lecture5/executor_demo_pkg/script/singlethreaded_executor.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import rclpy
+from executor_demo_pkg.executor_demo_interface import SingleThreadedExecutorInterface
+
+
+# def main(args=None):
+#     """
+#     Main function to initialize and run the ROS2 publisher node.
+
+#     Args:
+#         args (list, optional): Command-line arguments passed to the node. Defaults to None.
+#     """
+#     rclpy.init(args=args)
+#     node = SingleThreadedExecutorInterface("single_threaded_executor_demo")
+#     try:
+#         rclpy.spin(node)
+#     except KeyboardInterrupt:
+#         # Log a message when the node is manually terminated
+#         node.get_logger().warn("Keyboard interrupt detected")
+#     finally:
+#         # Cleanly destroy the node instance
+#         node.destroy_node()
+#         # Shut down the ROS 2 Python client library
+#         rclpy.shutdown()
+
+from rclpy.executors import SingleThreadedExecutor
+
+def main(args=None):
+    """
+    Main function to initialize and run the ROS2 publisher node.
+
+    Args:
+        args (list, optional): Command-line arguments passed to the node. Defaults to None.
+    """
+    rclpy.init(args=args)
+    node = SingleThreadedExecutorInterface("single_threaded_executor_demo")
+    executor = SingleThreadedExecutor()  # Explicit executor
+    executor.add_node(node)
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        # Log a message when the node is manually terminated
+        node.get_logger().warn("Keyboard interrupt detected")
+    finally:
+        # Cleanly destroy the node instance
+        node.destroy_node()
+        # Shut down the ROS 2 Python client library
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()  # Execute the main function when the script is run
